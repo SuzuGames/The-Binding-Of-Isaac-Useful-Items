cdebc7ce4ac4246b13ea98bd44aae047cb481aab
Suzu Games <79166566+SuzuGames@users.noreply.github.com>
2023-09-10 13:56:49 +0200

    Delete src/main directory

--- src/main/java/net/suzu/thebindingofisaac/procedures/BasementStructureBlockUpdateTickProcedure.java
+++
@@ -1,9 +0,0 @@
-package net.suzu.thebindingofisaac.procedures;
-
-import net.minecraft.world.level.LevelAccessor;
-
-public class BasementStructureBlockUpdateTickProcedure {
-	public static void execute(LevelAccessor world, double x, double y, double z) {
-		BasementProcedure.execute(world, x, y, z);
-	}
-}
